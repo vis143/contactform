7e56dafd0a845bfeff3b9b26ff95b0765d8568e3
vis143 <76201858+vis143@users.noreply.github.com>
2021-08-20 17:50:26 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,25 +1,17 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+//get data from form  
 
-    $email_from = 'vishalvis0607@gmail.com';
-
-    $email_subject = "new form";
-
-    $email_body = "user name: $name.\n".
-                    "user email: $email.\n".
-                     "user message: $message.";
-                
-
-                    
-    $to = "vishalchouhan0607@gmail.com";
-    $headers = "From: $email_from \r\n";
-            
-    $headers = "Reply-To: $email \r\n";
-                 
-    mail($to,$email_subject,$email_body,$headers);
-                 
-    header("Location: index.php");
-
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "vishalchouhan0607@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: vishalvis0607@gmail.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:form.html");
 ?>

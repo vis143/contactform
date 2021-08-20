5b78a47351a6d8bfc6225ad0c6a63214699b718a
vis143 <76201858+vis143@users.noreply.github.com>
2021-08-20 16:31:11 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,27 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    $email_from = 'vishalvis0607@gmail.com';
+
+    $email_subject = "new form";
+
+    $email_body = "user name: $name.\n".
+                    "user email: $email.\n".
+                    "user phone no.: $phone.\n".
+                     "user message: $message.\n";
+                
+
+                    
+    $to = "vishalchouhan0607@gmail.com";
+    $headers = "from: $email_from \r\n";
+            
+    $headers = "replt-to: $email \r\n";
+                 
+    mail($to,$email_subject,$email_body,$headers);
+                 
+    header("Location; contactform.html");
+
+?>
